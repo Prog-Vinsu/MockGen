95fbba63f81bde0dcbfae629f092b39df9f63c82
Prog-Vinsu <tognerepoloniniv@gmail.com>
2025-04-09 15:09:57 -0300

    :feat: add: adSense to layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <head>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1424643380498076"
+        crossorigin="anonymous"
+      />
+    </head>
     <NavBar />
     <NuxtPage />
     <AppFooter />
@@ -7,13 +14,13 @@
 </template>
 
 <script>
-import NavBar from '~/components/NavBar.vue'
-import AppFooter from '~/components/Footer.vue'
+import NavBar from "~/components/NavBar.vue";
+import AppFooter from "~/components/Footer.vue";
 
 export default {
   components: {
     NavBar,
-    AppFooter
+    AppFooter,
   },
-}
+};
 </script>
