f7c931ecf476382a5b602a2f867bb96eacfc298b
Prog-Vinsu <tognerepoloniniv@gmail.com>
2025-04-09 12:45:48 -0300

    :recycle: refactor: update nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import type { ModuleOptions } from "@nuxt/content";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
@@ -24,19 +23,12 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
   ],
-  content: {
-    documentDriven: true,
-    experimental: {
-      nativeSqlite: false
-    }
-  } as unknown as Partial<ModuleOptions>,
 
   vite: {
     plugins: [
